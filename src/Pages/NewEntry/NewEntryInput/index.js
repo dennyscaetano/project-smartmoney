6390974e77bcbf2c5a400cdb028b249263cb35e7
Dennys Matos <dennyscaetano@yahoo.com.br>
2024-11-04 14:33:54 -0300

    8.6.12 - Ajustando a Tela de Entrada (NewEntry) - Valor - Parte 2

--- src/Pages/NewEntry/NewEntryInput/index.js
+++ src/Pages/NewEntry/NewEntryInput/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {TextInputMask} from 'react-native-masked-text';
 
 import Colors from '../../../styles/Colors';
 
 const NewEntryInput = ({value, onChangeValue}) => {
   return (
-    <View>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.debitButton}>
+        <Text style={styles.debitButtonPrefix}>-</Text>
+        <Text style={styles.debitButtonText}>R$</Text>
+      </TouchableOpacity>
       <TextInputMask
         type={'money'}
         options={{
@@ -30,13 +34,33 @@ const NewEntryInput = ({value, onChangeValue}) => {
 export default NewEntryInput;
 
 const styles = StyleSheet.create({
-  input: {
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
     backgroundColor: Colors.asphalt,
     borderRadius: 15,
     marginHorizontal: 20,
     marginVertical: 10,
+  },
+  debitButton: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+  debitButtonPrefix: {
     fontSize: 28,
-    textAlign: 'center',
+    color: Colors.white,
+  },
+  debitButtonText: {
+    fontSize: 28,
+    color: Colors.white,
+  },
+  input: {
+    flex: 1,
+    paddingLeft: 0,
+    paddingRight: 20,
+    fontSize: 28,
+    textAlign: 'right',
     color: Colors.white,
   },
 });
