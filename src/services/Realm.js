111ca35cafb26940dc8bb60894da6d245643fe50
Dennys Matos <dennyscaetano@yahoo.com.br>
2024-11-04 14:36:43 -0300

    8.6.14 - Ajustando a Tela de Entrada (NewEntry) - Categorias - Parte 1

--- src/services/Realm.js
+++ src/services/Realm.js
@@ -3,11 +3,40 @@ import Realm from 'realm';
 import CategorySchema from '../schemas/CategorySchema';
 import EntrySchema from '../schemas/EntrySchema';
 
+import {getAllCategories} from './Categories';
+
 export const getRealm = async () => {
   const realm = await Realm.open({
     schema: [CategorySchema, EntrySchema],
-    schemaVersion: 1,
+    schemaVersion: 2,
   });
 
+  initDb(realm);
+
   return realm;
+};
+
+export const initDb = realm => {
+  const categoriesLength = realm.objects('Category').length;
+  console.log(`initDb :: Quantidade de categorias no BD: ${categoriesLength}`);
+
+  if (categoriesLength === 0) {
+    const categories = getAllCategories();
+
+    console.log(`initDB :: Initing db...`);
+
+    try {
+      realm.write(() => {
+        categories.forEach(category => {
+          console.log(
+            `initDB :: creating category: ${JSON.stringify(category)}`,
+          );
+
+          realm.create('Category', category, true);
+        });
+      });
+    } catch (error) {}
+  } else {
+    console.log(`initDB :: Categories already exists... Skipping`);
+  }
 };
