216a6acb1be00a21ef5d2c04858f3ce9b689e427
Dennys Matos <dennyscaetano@yahoo.com.br>
2024-11-04 13:55:59 -0300

    8.3.1 - O que são componentes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (__DEV__) {
 }
 
 import {AppRegistry} from 'react-native';
-import App from './src';
+import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
