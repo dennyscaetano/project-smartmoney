b9b932097b0f6d5d13dc5189899f161fe4279652
Dennys Matos <dennyscaetano@yahoo.com.br>
2024-11-04 14:39:32 -0300

    8.6.16 - Ajustando a Tela de Entrada (NewEntry) - Categorias - Parte 3

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import {name as appName} from './app.json';
 // Remover warning relacionado ao Async Storage
 YellowBox.ignoreWarnings([
   'Warning: Async Storage has been extracted from react-native core',
-  'Possible Unhandled Promise Rejection (id: 0)',
+  'Possible Unhandled Promise Rejection (id:',
 ]);
 
 AppRegistry.registerComponent(appName, () => App);
--- src/Pages/NewEntry/NewEntryCategoryPicker/index.js
+++ src/Pages/NewEntry/NewEntryCategoryPicker/index.js
@@ -1,10 +1,32 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, Modal} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Modal,
+  FlatList,
+  Alert,
+} from 'react-native';
+
+import {getAllCategories} from '../../../services/Categories';
 
 import Colors from '../../../styles/Colors';
 
 const NewEntryCategoryPicker = () => {
   const [modalVisible, setModalVisible] = useState(false);
+  const [allCategories, setAllCategories] = useState([]);
+
+  useEffect(() => {
+    async function loadCategories() {
+      const data = await getAllCategories();
+      setAllCategories(data);
+    }
+
+    loadCategories();
+
+    console.log('NewEntryCategoryPicker :: useEffect');
+  }, []);
 
   return (
     <View>
@@ -15,14 +37,29 @@ const NewEntryCategoryPicker = () => {
         }}>
         <Text style={styles.pickerButtonText}>Alimentação</Text>
       </TouchableOpacity>
+
       <Modal animationType="slide" transparent={false} visible={modalVisible}>
-        <Text>Modal</Text>
-        <TouchableOpacity
-          onPress={() => {
-            setModalVisible(false);
-          }}>
-          <Text>Fechar</Text>
-        </TouchableOpacity>
+        <View style={styles.modal}>
+          <FlatList
+            data={allCategories}
+            keyExtractor={item => item.id}
+            renderItem={({item}) => (
+              <TouchableOpacity style={styles.modalItem}>
+                <Text style={[styles.modalItemText, {color: item.color}]}>
+                  {item.name}
+                </Text>
+              </TouchableOpacity>
+            )}
+          />
+
+          <TouchableOpacity
+            style={styles.closeButton}
+            onPress={() => {
+              setModalVisible(false);
+            }}>
+            <Text style={styles.closeButtonText}>Fechar</Text>
+          </TouchableOpacity>
+        </View>
       </Modal>
     </View>
   );
@@ -31,6 +68,22 @@ const NewEntryCategoryPicker = () => {
 export default NewEntryCategoryPicker;
 
 const styles = StyleSheet.create({
+  modal: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+  modalItem: {
+    backgroundColor: Colors.asphalt,
+    padding: 20,
+    borderRadius: 15,
+    marginVertical: 10,
+    marginHorizontal: 20,
+  },
+  modalItemText: {
+    fontSize: 22,
+    textAlign: 'center',
+    color: Colors.white,
+  },
   pickerButton: {
     backgroundColor: Colors.asphalt,
     padding: 20,
@@ -42,5 +95,21 @@ const styles = StyleSheet.create({
     fontSize: 28,
     textAlign: 'center',
     color: Colors.white,
+  },
+  closeButton: {
+    backgroundColor: Colors.background,
+    paddingVertical: 3,
+    paddingHorizontal: 5,
+    borderWidth: 1,
+    borderColor: Colors.green,
+    borderRadius: 15,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    alignSelf: 'center',
+  },
+  closeButtonText: {
+    fontSize: 14,
+    color: Colors.green,
+    textAlign: 'center',
   },
 });
